d05ff3956485d7725915653832ca23c030645450
FilipSuvara <filip.suvara25112012@gmail.com>
2024-11-23 11:30:37 +0100

    racun,kasa

---
+++ racun.py
@@ -0,0 +1,6 @@
+kol-mleko=int (input("unesite koliko mleka"))
+cena-mleko=int(input("unesite koliko jedno kosta"))
+kol-hleb=int (input("unesite koliko hlebova"))
+cena-hleb=int(input("koliko kosta jedan"))
+budzet=("koliko imate para")
+ukupno=budzet - kol-mleko*cena-mleko+kol-hleb*cena-hleb
